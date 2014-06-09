32baf0344a06af62705984a85ca71d0c536fe1cc
Kris Winer <kriswiner@users.noreply.github.com>
2014-06-09 12:05:45 -0700

    Update MPU6050BasicExample.ino

--- MPU6050BasicExample.ino
+++ MPU6050BasicExample.ino
@@ -465,9 +465,6 @@ void LowPowerAccelOnlyMPU6050()
 void initMPU6050()
 {  
  // Initialize MPU6050 device
- // wake up device
-  writeByte(MPU6050_ADDRESS, PWR_MGMT_1, 0x00); // Clear sleep mode bit (6), enable all sensors 
-  delay(100); // Delay 100 ms for PLL to get established on x-axis gyro; should check for PLL ready interrupt  
 
  // get stable time source
   writeByte(MPU6050_ADDRESS, PWR_MGMT_1, 0x01);  // Set clock source to be PLL with x-axis gyroscope reference, bits 2:0 = 001
